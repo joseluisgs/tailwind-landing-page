e38028a363a0a542546f17606f192c40e12b8531
José Luis González <joseluisgonsan@gmail.com>
2021-08-25 18:05:07 +0200

    Transiciones

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
 // Tu código JS
 
-// Back To Top
+// BACK TO TOP Init
 addBackToTop({
   diameter: 56,
   backgroundColor: '#2563EB',
   textColor: '#F3F4F6'
 });
 
+// TRANSICIONES
+// en todos los href le apliacmos el estilo smooth
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
