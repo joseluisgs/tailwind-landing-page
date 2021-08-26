805048b11f1595b628e180be45e042c2a89e2ab5
José Luis González <joseluisgonsan@gmail.com>
2021-08-26 12:28:53 +0200

    Navbar

--- src/js/main.js
+++ src/js/main.js
@@ -48,3 +48,11 @@ function changeTheme() {
   if (currentTheme === 'light') setTheme('dark');
   else setTheme('light');
 };
+
+// NAVBAR PLUGIN
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
