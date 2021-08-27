ecb228ff01ef8bb390a692c57cf13767fb1cf872
José Luis González <joseluisgonsan@gmail.com>
2021-08-27 12:13:06 +0200

    Proyectos

--- src/js/main.js
+++ src/js/main.js
@@ -77,3 +77,36 @@ menuLinks.forEach(link => {
 function toggleMenu() {
   menu.classList.toggle("hidden");
 }
+
+// SLIDER
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("blogSlides");
+  let dots = document.getElementsByClassName("blogSlider__dots--dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
